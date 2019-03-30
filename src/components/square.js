84388562c0a614f1b9ac9462f402047961312147
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-03-29 23:24:19 -0400

    Refactor Square component into function component

--- src/components/square.js
+++ src/components/square.js
@@ -1,14 +1,8 @@
 import React from 'react';
 
-class Square extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {color: props.color};
-	}
+const Square = ({color}) => {
+	return <div className={"square square-" + color}/>;
+}
 
-	render(){
-		return <div className={"square square-" + this.state.color}/>
-	}
-} 
 
 export default Square;
