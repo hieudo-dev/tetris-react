0200c82cab2af2f7304aeebfd27b7ca17a5d6c9d
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-04-01 19:00:00 -0400

    Add Score Board component and some color changes

--- src/components/game.js
+++ src/components/game.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import Board from './board';
+import ScoreBoard from './score_board';
 
 class Game extends Component{
 	render() {
 		return <div className="game">
 			<Board/>
+			<ScoreBoard score={0} highscore={0} />
 		</div>
 	}
 } 
---
+++ src/components/score_board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ScoreBoard = ({score, highscore}) => {
+	return <div className="score-board">
+		<div className="score-label">SCORE</div>
+		<div className="score-value">{score}</div>
+		<div className="score-label">HIGHSCORE</div>
+		<div className="score-value">{highscore}</div>
+	</div>;
+}
+
+
+export default ScoreBoard;
