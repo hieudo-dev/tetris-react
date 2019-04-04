e374072baedebed6b21b51890e73dd9534003a6b
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-04-03 21:46:07 -0400

    Updates

---
+++ precache-manifest.baa86cbe331f19a0a5d80e32efade966.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db16a62f1a77f7f1df23",
+    "url": "/tetris-react/static/js/runtime~main.58e83ad5.js"
+  },
+  {
+    "revision": "b2090f86a8eeb508427b",
+    "url": "/tetris-react/static/js/main.1136df61.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/tetris-react/static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "b2090f86a8eeb508427b",
+    "url": "/tetris-react/static/css/main.cdb8de60.chunk.css"
+  },
+  {
+    "revision": "b907807ac442c69f9e5670ce81c8b575",
+    "url": "/tetris-react/index.html"
+  }
+];
