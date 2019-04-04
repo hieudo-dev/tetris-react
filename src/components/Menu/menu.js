106fba61decba4a26e9c5bcb1cf592d9ea7a6b2a
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-04-03 21:56:50 -0400

    Remove About button

--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -5,7 +5,6 @@ import MenuButton from '../MenuButton/menu_button';
 const Menu = ({startGame}) => {
 	return <div className="menu">
 		<MenuButton onClick={startGame} link="#" text="Play" color="green"></MenuButton>
-		<MenuButton link="#" text="About" color="light-blue"></MenuButton>
 	</div>;
 }
 
