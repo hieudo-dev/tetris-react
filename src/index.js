6d1d415ae052fce8feddfe8819f1ffc2cd8aa44b
Hieu Do Ngoc <hieudn.uh@gmail.com>
2020-05-29 20:56:19 -0400

    Set up project to use TailwindCSS

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import './sass/main.scss';
-import Menu from './components/Menu/menu';
-import Game from './components/Game/game';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./sass/main.scss";
+import "./assets/main.css";
+import Menu from "./components/Menu/menu";
+import Game from "./components/Game/game";
 
-class App extends Component{
-	constructor(props) {
-		super(props);
-		this.state = {playing: false}
-	}
+class App extends Component {
+   constructor(props) {
+      super(props);
+      this.state = { playing: false };
+   }
 
-	startGame() {
-		this.setState({playing: true})
-	}
+   startGame() {
+      this.setState({ playing: true });
+   }
 
-	render() {
-		return (<div>
-			{ this.state.playing ? <Game/>: <Menu startGame={() => this.startGame()}/> }
-		</div>)
-	}
+   render() {
+      return (
+         <div>
+            {this.state.playing ? (
+               <Game />
+            ) : (
+               <Menu startGame={() => this.startGame()} />
+            )}
+         </div>
+      );
+   }
 }
 
 //=================================>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
